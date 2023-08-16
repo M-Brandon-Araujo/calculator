8afc436101458765947907b3114e16cac07d4482
Brandon Araujo <brandonaraujo077@gmail.com>
2023-08-16 14:55:32 -0500

    Add functions for basic mathematical operations

---
+++ script.js
@@ -0,0 +1,25 @@
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
+//write readme and commit script and html
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(divide(3, 3));
+
+
